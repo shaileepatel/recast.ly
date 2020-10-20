e87a1d5728c3a160b9d550c93e03fa411b05049a
Shailee Patel <shaileeee@gmail.com>
2020-10-20 11:52:10 -0700

    work on test for api call

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
+var searchYouTube = function (options) {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: options.key,
+      q: options.query,
+      part: 'snippet',
+      maxResults: options.max,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    success: function(data) {
+      embedVideo(data);
+    },
+    error: function(response) {
+      console.log('Request Failed');
+    }
+  });
 };
 
 export default searchYouTube;
